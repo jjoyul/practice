e7d628a67516add59cdde32c2b90c98652e043f3
kevin06161 <kikkik019kik@gmail.com>
2019-08-31 18:02:13 +0900

    8/31(3)

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'mysite',
+    'books',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
