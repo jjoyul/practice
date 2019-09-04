176dbe5f6e8d3f28e9e209a52208d59eb1c246c9
kevin06161 <kikkik019kik@gmail.com>
2019-09-04 18:22:29 +0900

    9/4(1)

--- mysite/mysite/views.py
+++ mysite/mysite/views.py
@@ -1,8 +1,10 @@
-from django.http import HttpResponse, Http404
+from django.http import HttpResponse, Http404, HttpResponseRedirect
 from django.template.loader import get_template
 from django.template import Context
 from django.shortcuts import render
 import datetime
+from .forms import ContactForm
+from django.core.mail import send_mail
 
 def hello(request):
     return HttpResponse("Hello World")
@@ -13,3 +15,19 @@ def my_homepage_view(request):
 def current_datetime(request):
     now = datetime.datetime.now()
     return render(request, 'current.html', {'current_date': now})
+
+def contact(request):
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            send_mail(
+                cd['subject'],
+                cd['message'],
+                cd.get('email', 'noreply@example.com'),
+                ['siteowner@example.com'],
+            )
+            return HttpResponseRedirect('/contact/thanks/')
+    else:
+        form = ContactForm()
+    return render(request, 'contact_form.html', {'form': form})
